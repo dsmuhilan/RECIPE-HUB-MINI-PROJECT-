88d7490822108ff11018ba92c9da8836c7b75c34
dsmuhilan <125954828+dsmuhilan@users.noreply.github.com>
2023-07-20 10:25:03 +0530

    Add files via upload

---
+++ Day-7/src/Register.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+    const [number, setMobNo] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>REGISTER</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="password">Mobile No</label>
+            <input value={number} onChange={(e) => setMobNo(e.target.value)} type="number" placeholder="Your Mobile No" id="number" name="number" />
+            <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
